d430aae697aca87b6c219621c77b04a5371194f6
gubarbosa <gugabarbosa.bsi@gmail.com>
2021-02-18 17:51:53 -0300

    creating method getUniqueArrayPossibilities :rocket:

--- src/UniqueArrays/Array.java
+++ src/UniqueArrays/Array.java
@@ -3,11 +3,7 @@ import java.util.*;
 
 public class Array {
     public static void main(String[] args) {
-        Integer[] vetor ={1,2,2,4,4,15};
-        Integer[] vetor1 = {1,2,3,4,2,3};
-        Integer[][] vetor2 = {{1,2,3}, {2,3}, {3,4}};
-        List<List<Integer>> lista = new LinkedList<>();
-        Set<Integer[]> set = new HashSet<>(Arrays.asList(vetor2));
-        System.out.println(set);
-        }
+        Arrays list = new Arrays();
+        list.getUniqueArrayPossibilities();
     }
+}
---
+++ src/UniqueArrays/Arrays.java
@@ -0,0 +1,21 @@
+package UniqueArrays;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Arrays {
+    public String[][] array;
+    {
+        array = new String[][];
+    }
+    public Arrays() {
+    }
+
+    public void getUniqueArrayPossibilities() {
+        Set<String[]> set = new LinkedHashSet<>();
+        for (String[] valor: array) {
+            set.add(valor);
+        }
+        System.out.println(set);
+    }
+}
